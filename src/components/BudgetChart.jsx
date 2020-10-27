5a1b7ef5bb1eae5a545d44394ce5db9c1720eae4
Joe Dimartino <j.dimartino11@gmail.com>
2020-10-27 14:51:13 -0400

    Refactor Budget Chart
    Budget Chart will now only worry about rendering the chart based on data
    provided to it.  Other logic will live in BUdgetWidget

--- src/components/BudgetChart.jsx
+++ src/components/BudgetChart.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Inputs from './Inputs.jsx';
 import {Bar} from 'react-chartjs-2';
 
 class BudgetChart extends React.Component {
@@ -8,21 +7,8 @@ class BudgetChart extends React.Component {
 
     this.state = {
       charData:{
-          labels: ['Jan','Feb', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
-        datasets:[
-            {
-              label: ['Income'],
-              data: [1000,800,1000,800,1000,1000,1000,1000,1000,1000,1000,1000],
-              backgroundColor: 'rgba(255, 99, 132, 0.2)',
-              borderColor: 'rgb(0, 0, 0)'
-            },
-            {
-              label: ['Expense'],
-              data: [500,1200,500,500,500,500,600,500,500,500,500,500],
-              backgroundColor: 'rgba(100, 50, 70, 0.2)',
-              borderColor: 'rgb(0, 0, 0)'
-            }
-          ]
+          labels: this.props.labels,
+        datasets: this.props.data
         },
         charOptions: {
           maintainAspectRatio: true,
@@ -44,14 +30,11 @@ class BudgetChart extends React.Component {
 
   render() {
     return (
-    <div>
       <div className="chart">
         <Bar
           data={this.state.charData}
           options={this.state.charOptions}/>
       </div>
-      <Inputs/>
-    </div>
   )}
 }
 
