af4595fc62eae67ed1748643ed8e4f0aa39443a1
Joe Dimartino <j.dimartino11@gmail.com>
2020-10-27 22:28:12 -0400

    Factored out BudgetWidget component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Navigation from './components/Navigation.jsx';
-import BudgetWidgetContainer from './redux/containers/BudgetWidgetContainer.js';
+import InputsContainer from './redux/containers/InputsContainer.js';
+import BudgetChartContainer from './redux/containers/BudgetChartContainer.js';
 
 
 var App = (props) => {
   return (
     <div>
       <Navigation/>
-      <BudgetWidgetContainer/>
+      <BudgetChartContainer />
+      <InputsContainer/>
     </div>
   )
 }
