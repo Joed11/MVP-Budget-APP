3ebe869a94d997ad1602a850a16fe72b5044ec17
Joe Dimartino <j.dimartino11@gmail.com>
2020-10-28 13:30:42 -0400

    Refactor to update chart based on user input
    MakeNewEntry function now assigns an id based being at the end of the
    existing dataPoint array
    
    Hooks are now used to maintain the currentChart data used to generate
    the initial set of entry fields as well as the newChart being
    constructed as the user adds fields and alters data.
    
    Passes a method down to InputField components so that they may update
    the newChart living within this component as the user alters data

--- src/components/InputList.jsx
+++ src/components/InputList.jsx
@@ -1,17 +1,59 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import InputField from './InputField.jsx';
 
 var InputList = (props) => {
-  var counter = 0;
-  return props.dataList.map((entry) => {
-    return <InputField
-      id={counter++}
-      amount={entry.amount}
-      transactionType={entry.transactionType}
-      category={entry.category}
-      months={entry.months}
-      categories={props.categories}/>
-  })
+
+  const [chartData, setChartData] = useState(props.dataPoints);
+  const [newChartData, setNewChartData] = useState(props.dataPoints);
+
+  useEffect(() => {
+    console.log('newChartdatachanged', newChartData)
+  }, [newChartData])
+
+  return (
+  <div className="data">
+    {props.dataPoints.map((entry) => {
+      return <InputField
+        id={entry.id}
+        amount={entry.amount}
+        transactionType={entry.transactionType}
+        category={entry.category}
+        months={entry.months}
+        categories={props.categories}
+        updateData={setNewChartData.bind(this)}
+        currentData={newChartData}/>
+    })}
+    <button onClick={() => {
+        var newDataPoints = props.dataPoints.slice(0);
+        var newDataEntry = makeNewEntry(newChartData);
+        newDataPoints.push(newDataEntry)
+        props.updateDataPoints(newDataPoints);
+        console.log(newDataPoints)
+      }}>Add New Item</button>
+      <button onClick={() => {
+        props.updateDataPoints(newChartData);
+      }}>Update Chart</button>
+  </div>
+  )
 }
 
 export default InputList;
+
+var incomeColors = ['rgba(51,204,255,0.7)', 'rgba(51,204,51,0.7)', 'rgba(51,204,204,0.7)', 'rgba(153,153,255,0.7)', 'rgba(0,102,153,0.7)'];
+
+var makeNewEntry = function (newChartData) {
+  var randomIdx = Math.floor(Math.random() * Math.floor(4))
+  var obj = {
+  chartData: {
+    label: ['Misc.'],
+    data: [0,0,0,0,0,0,0,0,0,0,0,0],
+    backgroundColor: incomeColors[randomIdx],
+    borderColor: 'rgb(255, 255, 255)'
+  },
+  id: newChartData.length,
+  amount: 0,
+  transactionType: { value: 'Income', label: 'Income' },
+  category: { value: 'Misc.', label: 'Misc.' }
+}
+return obj;
+}
