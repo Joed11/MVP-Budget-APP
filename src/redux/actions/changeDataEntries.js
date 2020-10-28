ec5567cb54dfe6b3714c4513f64d1a04ac3cb3e9
Joe Dimartino <j.dimartino11@gmail.com>
2020-10-27 22:25:48 -0400

    Adjust name of reducer and actions to change dataPoints

--- src/redux/actions/changeDataEntries.js
+++
@@ -1,6 +0,0 @@
-var changeDataEntries = (entries) => ({
-  type: "CHANGE_DATA_ENTRIES",
-  payload: entries
-});
-
-export default changeDataEntries;
--- src/redux/reducers/main.js
+++ src/redux/reducers/main.js
@@ -1,13 +1,13 @@
 import { combineReducers } from 'redux';
 import xLabelsReducer from './xLabelsReducer.js';
-import dataEntriesReducer from './dataEntriesReducer.js';
+import dataPointsReducer from './dataPointsReducer.js';
 import assetsReducer from './assetsReducer.js';
 import savedChartsReducer from './savedChartsReducer.js';
 import categoriesReducer from './categoriesReducer.js';
 
 var rootReducer = combineReducers({
   xLabels: xLabelsReducer,
-  dataEntries: dataEntriesReducer,
+  dataPoints: dataPointsReducer,
   assets: assetsReducer,
   savedCharts: savedChartsReducer,
   categories: categoriesReducer
