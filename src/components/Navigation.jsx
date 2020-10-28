34a9c52222de6f596ef10bdf55f0a957268f3753
Joe Dimartino <j.dimartino11@gmail.com>
2020-10-28 16:26:15 -0400

    Add fields and skelton of data handling
    Adds username input - this will be used to reference users charts from
    the database
    
    Add Get charts button this will retrieve any saved charts associated
    with this user from the database
    
    Add starting asset input - this is used to set the asset value within
    the store - changing this will cause the chart to re-render the chart
    based on the new asset data
    
    Add my charts drop down - if the user has charts to retrieve this will
    be populated with their charts - selecting a chart will swap out the
    chart data in the store with the saved data
    
    Adds chart save fields - this will allow a user to enter a name for the
    chart and submitting to the database - the chart will be associated with
    their username - Entering the username and hitting get my charts will
    retrieve form the database

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {useState , useEffect} from 'react';
 
 var Navigation = (props) => {
+  const [assets, setAssets] = useState(props.assets);
+  const [username, setUsername] = useState(props.username);
+  const [savedCharts, setSavedCharts] = useState(props.savedCharts);
+  const [chartName, setChartName] = useState('');
+
+  useEffect(() => {
+    props.updateAssets(assets);
+  }, [assets])
+
+
   return (
-    <div>
-      Im a navigation component
+    <div className="nav-container">
+      <div className="navigation-container">
+        <p className="input-nav-label">Username:</p>
+        <input className="input-nav-input" type='text' onChange={(e)=> setUsername(e.target.value)} value={username}></input>
+        <button className="input-nav-button">GET MY CHARTS</button>
+        <p className="input-nav-label">Starting Assets:</p>
+        <input className="input-nav-input" type='number' onChange={(e)=> setAssets(e.target.value)} value={assets}></input>
+        <p className="input-nav-label">My Charts</p>
+        <select className="input-nav-input nav-charts">
+          <option>None</option>
+        </select>
+      </div>
+      <p className="input-nav-chartName-label">NAME THIS CHART</p>
+      <input className="input-nav-chartName" type='test' onChange={(e)=> setChartName(e.target.value)} value={chartName}></input>
+      <button className="input-nav-save-button">SAVE CURRENT CHART</button>
     </div>
   )
 }
