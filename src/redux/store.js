7a42841ebd0e7ef7fe7501e1d26d48367101a5dc
Joe Dimartino <j.dimartino11@gmail.com>
2020-10-27 22:23:18 -0400

    Refactor default state structure to be more clear
    Organzed dummy data based on needs of chart and unput system.  Provided
    a basic set of dummy data to load when a user loads the page.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,49 +1,113 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/main.js';
 
+var incomeColors = ['rgba(51,204,255,0.7)', 'rgba(51,204,51,0.7)', 'rgba(51,204,204,0.7)', 'rgba(153,153,255,0.7)', 'rgba(0,102,153,0.7)'];
 
-var defaultDates = ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+var expenseColors = ['rgba(255,0,0,0.7)', 'rgba(204,102,0,0.7)', 'rgba(153,0,153,0.7)', 'rgba(255,102,0,0.7)', 'rgba(255,204,0,0.7)'];
 
-var incomeColors = ['rgba(51,204,255,0.7)', 'rgba(51,204,51,0.7)', 'rgba(51,204,204,0.7)', 'rgba(153,153,255,0.7)', 'rgba(0,102,153,0.7)']
-
-var expenseColors = ['rgba(255,0,0,0.7)', 'rgba(204,102,0,0.7)', 'rgba(153,0,153,0.7)', 'rgba(255,102,0,0.7)', 'rgba(255,204,0,0.7)']
-
-var defaultAssets = 50000;
-
-var defaultData = [
-  {
-    label: ['Income'],
-    data: [1000,1000,1000,1000,1000,1000,1000,1000,0,1000,1000,1000],
-    backgroundColor: incomeColors[0],
-    borderColor: 'rgb(255, 255, 255)'
-  },
-  {
-    label: ['Expense'],
-    data: [500,500,500,0,500,500,500,0,500,500,500,500],
-    backgroundColor: expenseColors[1],
-    borderColor: 'rgb(255, 255, 255)'
-  },
-  {
-    label: ['Another One'],
-    data: [200,500,300,100,1000,200,50,0,100,500,25,500],
-    backgroundColor: expenseColors[2],
-    borderColor: 'rgb(255, 255, 255)'
-  }
-];
-
-var defaultCategories = ['Takout','Groceries','Rent','Mortgage','Entertainment','Subscriptions','Recreation','Utilities'];
-
-var defaultState = {
-  xLabels: defaultDates,
-  dataEntries: defaultData,
-  incomeColors: incomeColors,
-  expenseColors: expenseColors,
-  assets: defaultAssets,
-  savedCharts: [],
-  categories: defaultCategories
+var defaultChart = {
+  dates: ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
+  assets: 50000,
+  dataPoints: [
+    {
+      chartData: {
+        label: ['Salary'],
+        data: [1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000],
+        backgroundColor: incomeColors[0],
+        borderColor: 'rgb(255, 255, 255)'
+      },
+      amount: 1000,
+      transactionType: { value: 'Income', label: 'Income' },
+      category: { value: 'Salary', label: 'Salary' },
+      months: {
+        'Jan': true,
+        'Feb': true,
+        'Mar': true,
+        'Apr': true,
+        'June': true,
+        'July': true,
+        'Aug': true,
+        'Sep': true,
+        'Oct': true,
+        'Nov': true,
+        'Dec': true,
+      }
+    },
+    {
+      chartData: {
+        label: ['Rent'],
+        data: [-500,-500,-500,-500,-500,-500,-500,-500,-500,-500,-500,-500],
+        backgroundColor: expenseColors[1],
+        borderColor: 'rgb(255, 255, 255)'
+      },
+      amount: 500,
+      transactionType: { value: 'Expense', label: 'Expense' },
+      category: { value: 'Rent', label: 'Rent' },
+      months: {
+        'Jan': true,
+        'Feb': true,
+        'Mar': true,
+        'Apr': true,
+        'June': true,
+        'July': true,
+        'Aug': true,
+        'Sep': true,
+        'Oct': true,
+        'Nov': true,
+        'Dec': true,
+        'All': true,
+      }
+    },
+    {
+      chartData: {
+        label: ['Utilities'],
+        data: [-200,-200,-200,-200,-200,-200,-200,-200,-200,-200,-200,-200],
+        backgroundColor: expenseColors[2],
+        borderColor: 'rgb(255, 255, 255)'
+      },
+      amount: 200,
+      transactionType: { value: 'Expense', label: 'Expense' },
+      category: { value: 'Utilities', label: 'Utilities' },
+      months: {
+        'Jan': true,
+        'Feb': true,
+        'Mar': true,
+        'Apr': true,
+        'June': true,
+        'July': true,
+        'Aug': true,
+        'Sep': true,
+        'Oct': true,
+        'Nov': true,
+        'Dec': true,
+      }
+    }
+  ],
+  categories: [
+    { value: 'Salary', label: 'Salary' },
+    { value: 'Takeout', label: 'Takeout' },
+    { value: 'Groceries', label: 'Groceries' },
+    { value: 'Rent', label: 'Rent' },
+    { value: 'Mortgage', label: 'Mortgage' },
+    { value: 'Entertainment', label: 'Entertainment' },
+    { value: 'Subscriptions', label: 'Subscriptions' },
+    { value: 'Recreation', label: 'Recreation' },
+    { value: 'Utilities', label: 'Utilities' },
+    { value: 'Misc.', label: 'Misc.' }
+  ]
 }
 
-var store = createStore(rootReducer, defaultState, applyMiddleware(thunk));
+var defaultState = {
+  xLabels: defaultChart.dates,
+  dataPoints: defaultChart.dataPoints,
+  incomeColors: incomeColors,
+  expenseColors: expenseColors,
+  assets: defaultChart.assets,
+  savedCharts: [],
+  categories: defaultChart.categories
+}
+
+var store = createStore(rootReducer, defaultState);
 
 export default store;
