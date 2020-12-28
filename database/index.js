01779f6ea7609e6df940de9fcf2e3389a58d3f6c
Joe Dimartino <j.dimartino11@gmail.com>
2020-12-28 11:41:05 -0500

    Update db connection to use permanant deployed db

--- database/index.js
+++ database/index.js
@@ -1,10 +1,12 @@
 var mongoose = require('mongoose');
-var dbHost = process.env.DB_HOST || '127.0.0.1:27017';
-var dbName = process.env.DB_NAME || 'MVP';
+var dbHost = process.env.DB_HOST;
+var dbName = process.env.DB_NAME;
+var dbUser = process.env.DB_USER;
+var dbPw = process.env.DB_PASSWORD;
 
-mongoose.connect(`mongodb://${dbHost}/${dbName}`, { useNewUrlParser: true })
+mongoose.connect(`mongodb://${dbUser}:${dbPw}@${dbHost}/${dbName}?authSource=admin`, { useNewUrlParser: true })
   .then(()=> {console.log('Connected to the db')})
-  .catch(error => handleError(error));
+  .catch(error => console.log('error connecting to db', error));
 
   const chartSchema = new mongoose.Schema({
     username: String,
@@ -19,3 +21,4 @@ mongoose.connect(`mongodb://${dbHost}/${dbName}`, { useNewUrlParser: true })
    const Chart = mongoose.model('charts', chartSchema);
 
    module.exports.Chart = Chart;
+
